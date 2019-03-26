83d7dd2843fa5fae6d1aa87b7d48aabec49191b7
Adam Pavlicek <adamhpavlicek@gmail.com>
2019-03-26 14:28:31 -0500

    add filter func

--- higher_order_func_drills.js
+++ higher_order_func_drills.js
@@ -1,7 +1,8 @@
 'use strict';
+// repeat function drill
 function repeat(fn, n){
     // eslint-disable-next-line no-undef
-    for (i = 0; i < n; i++) {
+    for ( let i = 0; i < n; i++) {
         fn();
     }
 }
@@ -17,3 +18,33 @@ const goodbye = () => console.log('Goodbye World');
 
 repeat(hello, 5);
 repeat(goodbye, 5);
+
+// filter funciton drill
+
+function filter(arr, fn) {
+    let newArray = [];
+    for(let i = 0; i< arr.length; i++){
+        if(fn(arr[i]) === true){
+            newArray.push(arr[i]);
+        }
+    }
+    return newArray;
+    //TASK: Define your function here
+}
+
+//const filter = (arr, fn) => ASK MENTORS!!!
+
+// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// First we setup an array of strings we plan to filter:
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+// We use your `filter` function here, capturing a new array into `filteredNames`
+// comprised of names that only begin with 'R'
+const filteredNames = filter(myNames, function(name) {
+    // This is known as a "predicate function" - it's a function that 
+    // only returns a boolean
+    return name[0] === 'R';
+});
+
+console.log(filteredNames); // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
